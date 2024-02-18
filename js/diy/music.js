a1cb2c30f7ea1f80a92363b11a1867fda359d2c2
ty-leo <318498980@qq.com>
2024-02-18 20:07:40 +0800

    Site updated: 2024-02-18 20:07:40

--- js/diy/music.js
+++ js/diy/music.js
@@ -16,25 +16,25 @@ const ap = new APlayer({
         {
             name: "Take me Hand",
             artist: 'Cécile Corbel',
-            url: 'https://nf-sycdn.kuwo.cn/161f4edbf309527a7284755fb8c31f1c/65b317df/resource/n2/31/83/2354335626.mp3',
+            url: 'https://nf-sycdn.kuwo.cn/31b0ad0d8d9635e0125deddc9c8d3e71/65cdfcf8/resource/n2/31/83/2354335626.mp3?from=vip',
             cover: 'https://img1.kuwo.cn/star/albumcover/500/21/99/3057700581.jpg',	
         },
         {
             name: "去年夏天",
             artist: '王大毛',
-            url: 'https://sp-sycdn.kuwo.cn/888039be5cb37fde0b1d20a048af616f/65b5116d/resource/n3/42/78/300290220.mp3',
+            url: 'https://sp-sycdn.kuwo.cn/103ed12f819ddf7325204dcebfc55e8f/65ce138b/resource/n3/42/78/300290220.mp3?from=vip',
             cover: 'https://y.qq.com/music/photo_new/T002R300x300M000003eyd0o3lYmxM_1.jpg?max_age=2592000',	
         },
         {
             name: "世界那么大还是遇见你",
             artist: '程响',
-            url: 'https://cl-sycdn.kuwo.cn/a658f936b3a8b4dc9521e81700d3092f/65b512e6/resource/n1/64/17/332703764.mp3',
+            url: 'https://cl-sycdn.kuwo.cn/799c7d89feffc19bd504367d0ef78aa6/65ce13cb/resource/n1/64/17/332703764.mp3?from=vip',
             cover: 'https://y.qq.com/music/photo_new/T002R300x300M000003Ls5Jo4EFBIH_3.jpg?max_age=2592000',	
         },
         {
             name: "一个人挺好",
             artist: '孟颖',
-            url: 'https://www.shiyinren.com/server/1/176186706.mp3',
+            url: 'https://www.shiyinren.com/server/1/176186706.mp3',  //https://eb-sycdn.kuwo.cn/ee8768772362e44f99aebb503604fee5/65ce146b/resource/n3/92/87/2582455809.mp3?from=vip
             cover: 'https://imgessl.kugou.com/stdmusic/20191224/20191224170717617635.jpg',	
         }
     ]
